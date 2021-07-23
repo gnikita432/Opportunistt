f1a27fb6efdab9cc2302dea9853936cafbb09990
Nikita Gupta <gnikita432@gmail.com>
2021-07-23 13:30:00 +0530

    Installed client side dependencies and setup folders

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// this 👆 instead of const express=require('express'); for importing (mordern nd easier to use) you can enable it by going to package.json and one line bellow main that is type:module.
+
