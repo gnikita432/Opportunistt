f126e38de3394324b68b78c19668e2870b09a759
Nikita Gupta <gnikita432@gmail.com>
2021-07-31 17:51:54 +0530

    jsx for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,30 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography varaint="h2" align="center">
+          Opportunities
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing="{3}"
+          >
+            <Grid item xs={12} sm={7}>
+              {/* <Opportunity /> */}
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* <Form /> */}
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
