9365021d680ea8ea242b437b297332c04ea5861b
Nikita Gupta <gnikita432@gmail.com>
2021-08-04 13:57:26 +0530

    reducer set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "react-redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import reducers from "./reducers";
+
+import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import opportunities from "./opportunities";
+
+export default combineReducers({
+  opportunities,
+});
