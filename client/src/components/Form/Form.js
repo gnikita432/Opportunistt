9908b0ae127fcf77689a30517d6adc8e87276ae0
Nikita Gupta <gnikita432@gmail.com>
2021-08-07 16:02:34 +0530

    Implemented Form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 const Form = () => {
-  return <h1>Form</h1>;
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    link: "",
+    applyby: "",
+    tags: "",
+  });
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
+  return (
+    <Paper>
+      <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Typography variant="h5">Post an Opportunity</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="link"
+          variant="outlined"
+          label="Link"
+          fullWidth
+          value={postData.link}
+          onChange={(e) => setPostData({ ...postData, link: e.target.value })}
+        />
+        <TextField
+          name="applyby"
+          variant="outlined"
+          label="Apply By"
+          fullWidth
+          value={postData.applyby}
+          onChange={(e) =>
+            setPostData({ ...postData, applyby: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
