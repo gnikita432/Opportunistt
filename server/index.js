4df14fb7a08d21d6cb47dcbf9ae67398a8319ab1
Nikita Gupta <gnikita432@gmail.com>
2021-08-21 11:34:54 +0530

    Added environmental variables

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 // this 👆 instead of const express=require('express'); for importing (mordern nd easier to use) you can enable it by going to package.json and one line bellow main that is type:module.
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "25mb", extended: true }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
@@ -16,12 +18,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Nikita:Nikita@cluster0.ps0ma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`server running on port Successfuly ${PORT}`)
